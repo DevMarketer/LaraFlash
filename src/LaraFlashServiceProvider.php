ba854919bf4434e94363800d5fd125ca972f1206
J. Alexander Curtis <jalexandercurtis@gmail.com>
2017-09-05 10:15:20 -0600

    Initial Commit

---
+++ src/LaraFlashServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace DevMarketer\LaraFlash;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaraFlashServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
